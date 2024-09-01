c284a052ee4438b118d9c45ae912c089c735cdaf
Hackingshelby <hackingthomas00@gmail.com>
2024-09-01 02:02:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,13 +8,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "237696900612"
+global.ownernomer = "tu ajoutes ton numéro ici"
 global.ownername = "*Hans Tech"
 global.ytname = "YT: HansTech0"
 global.socialm = ""
 global.location = "EARTH,AFRICA"
 
-global.ownernumber = '237696900612'  //creator number
+global.ownernumber = 'ici aussi tu ajoute ton numéro'  //creator number
 global.ownername = 'Harold MTH' //owner name
 global.botname = 'Hans-Bug-bot' //name of the bot
 
